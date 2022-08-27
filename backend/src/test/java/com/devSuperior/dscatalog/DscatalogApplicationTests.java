e09a9c8126d233ce87b9e20feaf9983cc08c0caa
Shermannz <robcfaraco@outlook.com>
2022-08-27 16:15:53 -0300

    backend project created

---
+++ backend/src/test/java/com/devSuperior/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
