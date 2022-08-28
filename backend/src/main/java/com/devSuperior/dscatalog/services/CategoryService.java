f8ca969293fabf2e34cc2b4b881acaee37182f92
Shermannz <robcfaraco@outlook.com>
2022-08-28 18:17:16 -0300

    exception

--- backend/src/main/java/com/devSuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devSuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,7 @@
 package com.devSuperior.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devSuperior.dscatalog.dto.CategoryDTO;
 import com.devSuperior.dscatalog.entities.Category;
 import com.devSuperior.dscatalog.repositories.CategoryRepository;
+import com.devSuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -27,6 +27,13 @@ public class CategoryService {
 		.map(x -> new CategoryDTO(x))
 		.collect(Collectors.toList());
 		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+       Optional<Category> obj = repository.findById(id);
+	   Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+	   return new CategoryDTO(entity);
 	}
 	
 }
